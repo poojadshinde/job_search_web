4cd100db384f46c1f1f1b23ee0d7fe1368f052b3
poojadshinde <poojadshinde099@gmail.com>
2018-09-07 12:19:34 -0700

    second commit

--- application/controllers/job_seeker.php
+++ application/controllers/job_seeker.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Job_seeker extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
+	 *  for this controller.
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
